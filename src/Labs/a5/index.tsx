adeb307248a715dfcf3fd390e46998e2154030f3
christinalizx <christinalizx@gmail.com>
2024-03-28 16:39:10 -0400

    A5 Commit till 3.4

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -1,5 +1,6 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 function Assignment5() {
     return (
       <div>
@@ -9,6 +10,7 @@ function Assignment5() {
         </a>
         <EncodingParametersInURLs />
         <WorkingWithObjects/>
+        <WorkingWithArrays /> 
       </div>
     );
   }
