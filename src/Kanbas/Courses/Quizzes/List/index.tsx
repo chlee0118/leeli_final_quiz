1c9734fd030c030a921005412a7a8e91bf4fcb1b
christinalizx <christinalizx@gmail.com>
2024-04-18 16:46:27 -0400

    First Commit

---
+++ src/Kanbas/Courses/Quizzes/List/index.tsx
@@ -0,0 +1,217 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  FaBan,
+  FaCalendar,
+  FaCheckCircle,
+  FaEllipsisV,
+  FaRocket,
+  FaPlus,
+  FaPlusCircle, 
+} from "react-icons/fa";
+import { IQuiz } from "../client";
+import * as client from "../client";
+
+export default function QuizList() {
+  const today = new Date();
+  const navigate = useNavigate();
+  const { courseId } = useParams();
+  useEffect(() => {
+    client.findQuizByCourseId(courseId).then((quizzes) => setQuizzes(quizzes));
+  }, [courseId]);
+
+  const [quizzes, setQuizzes] = useState<IQuiz[]>([]);
+  const [quizzesExist, setQuizzesExist] = useState<boolean>(false);
+  const [selectedQuiz, setSelectedQuiz] = useState<IQuiz | null>(null);
+  const [menuVisible, setMenuVisible] = useState<boolean>(false);
+
+  const handleAddQuizClick = () => {
+    navigate(`/Kanbas/Courses/${courseId}/Quizzes/new`);
+  };
+  
+  const handleNewQuiz = () => {
+    const defaultDate = new Date().getDate();
+
+    const newQuiz = {
+      title: "New Quiz",
+      quizType: "Graded Quiz",
+      points: 0,
+      assignmentGroup: "Quizzes",
+      shuffleAnswers: true,
+      timeLimit: 20,
+      multipleAttempts: false,
+      showCorrectAnswers: false,
+      accessCode: "",
+      oneQuestionAtATime: true,
+      webcamRequired: false,
+      lockQuestionsAfterAnswering: false,
+      dueDate: defaultDate,
+      availableDate: defaultDate,
+      untilDate: defaultDate,
+      courseId: courseId,
+      published: false,
+    };
+
+    client.createQuiz(courseId, newQuiz).then((q) => {
+      navigate(`/Kanbas/Courses/${courseId}/Quizzes/${q._id}/Edit`);
+    });
+  };
+
+  const handleEditQuiz = (quiz: IQuiz) => {
+    navigate(`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}/Edit`);
+  };
+
+  const handleDeleteQuiz = (quiz: IQuiz) => {
+    client.deleteQuiz(quiz).then(() => {
+      setQuizzes(quizzes.filter((q) => q._id !== quiz._id));
+    });
+  };
+
+  // handle quiz publish
+  const handlePublishQuiz = (quiz: IQuiz) => {
+    const updatedQuiz = { ...quiz, published: !quiz.published };
+    client.updateQuiz(updatedQuiz).then(() => {
+      setQuizzes((prevQuizzes) =>
+        prevQuizzes.map((q) => (q._id === updatedQuiz._id ? updatedQuiz : q))
+      );
+      setMenuVisible(!menuVisible);
+    });
+  };
+
+  const toggleMenu = (quiz: IQuiz) => {
+    setSelectedQuiz(quiz);
+    setMenuVisible(!menuVisible);
+  };
+
+  useEffect(() => {
+    const quizzesExist = quizzes.length > 0;
+    setQuizzesExist(quizzesExist);
+  }, [quizzes]);
+
+  return (
+    <div
+      className="overflow-y-scroll position-fixed bottom-0 end-0"
+      style={{ left: "320px", top: "250px" }}
+    >
+        <div className="container">
+                <div className="d-flex justify-content-between align-items-center">
+          <form className="form-inline">
+            <input
+              id="text-fields-SA"
+              className="form-control"
+              placeholder="Search Assignment"
+            />
+          </form>
+
+          <div className="float-end">
+
+            <button className="btn btn-primary float-end" onClick={handleNewQuiz}>
+          <FaPlus /> Quiz
+        </button>
+            <button className="btn btn-outline-primary me-2"> ︙</button>
+          </div>
+        </div>
+
+      <div className="container">
+      <ul className="list-group wd-modules">
+          <li className="list-group-item">
+            <div>
+              <FaEllipsisV className="me-2" /> ASSIGNMENT QUIZZES
+              <span className="float-end">
+                <FaCheckCircle className="text-success" />
+                <FaPlusCircle className="ms-2" />
+                <FaEllipsisV className="ms-2" />
+              </span>
+            </div>
+        <ul className="list-group">
+          {quizzes.map((quiz) => (
+            <li className="list-group-item" key={quiz._id}>
+              <div className="container">
+                <div className="row">
+                  <div className="col-8">
+
+                  {quiz.published ? (
+                      <FaRocket className="text-success" style={{marginRight: 10}} />
+                    ) : (
+                      <FaRocket className="text-secondary" style={{marginRight: 10}} />
+                    )}
+                    <Link
+                      to={`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}`}
+                    >
+                      {quiz.title}
+                    </Link>
+                  </div>
+                  <div className="col-4 text-right">
+                    {quiz.published ? (
+                      <FaCheckCircle className="float-end text-success ms-2" />
+                    ) : (
+                      <FaBan className="float-end text-danger ms-2" />
+                    )}
+                    <FaEllipsisV
+                      className="ms-2 float-end"
+                      onClick={() => toggleMenu(quiz)}
+                    />
+                  </div>
+                </div>
+                <div className="row mt-2">
+                  <div className="col-md-4">
+                    {new Date(quiz.availableDate) > today
+                      ? "Not available until " +
+                        new Date(quiz.availableDate).toLocaleDateString()
+                      : new Date(quiz.dueDate) > today
+                      ? "Available"
+                      : "Closed"}
+                  </div>
+                  <div className="col-md-4">
+                    <FaCalendar className="text-secondary" />
+                    {" Due Date: " +
+                      new Date(quiz.dueDate).toLocaleDateString()}
+                  </div>
+                  <div className="col-md-4">
+                    {quiz.points + " pts" + "｜ 0 Questions"}
+                  </div>
+                </div>
+                {menuVisible && selectedQuiz === quiz && (
+                  <div className="row">
+                    <div className="col text-right">
+                      <button
+                        className="btn btn-sm btn-success me-2"
+                        onClick={() => handleEditQuiz(quiz)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-sm btn-danger"
+                        onClick={() => handleDeleteQuiz(quiz)}
+                      >
+                        Delete
+                      </button>
+                      {quiz.published ? (
+                        <button
+                          className="btn btn-sm btn-secondary me-2"
+                          onClick={() => handlePublishQuiz(quiz)}
+                        >
+                          Unpublish
+                        </button>
+                      ) : (
+                        <button
+                          className="btn btn-sm btn-info me-2"
+                          onClick={() => handlePublishQuiz(quiz)}
+                        >
+                          Publish
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+        </li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Kanbas/Courses/Quizzes/client.tsx
@@ -0,0 +1,97 @@
+import axios from "axios";
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const COURSES_API = `${API_BASE}/api/courses`;
+export const QUIZZES_API = `${API_BASE}/api/quizzes`;
+export const QUESTIONS_API = `${API_BASE}/api/questions`;
+
+const api = axios.create({
+  withCredentials: true,
+});
+
+export interface IQuiz {
+  _id: string;
+  title: string;
+  quizType: string;
+  points: number;
+  assignmentGroup: string;
+  shuffleAnswers: boolean;
+  timeLimit: number;
+  multipleAttempts: boolean;
+  showCorrectAnswers: boolean;
+  accessCode: string;
+  oneQuestionAtATime: boolean;
+  webcamRequired: boolean;
+  lockQuestionsAfterAnswering: boolean;
+  dueDate: Date;
+  availableDate: Date;
+  untilDate: Date;
+  courseId: string;
+  published: boolean;
+}
+
+export interface IQuestion {
+  _id: string;
+  quizId: string;
+  questionType: string;
+  title: string;
+  points: string;
+  question: string;
+  choices: string[];
+  correctAnswerIndex: number;
+}
+
+export const createQuiz = async (courseId: any, quiz: any) => {
+  const response = await api.post(`${COURSES_API}/${courseId}/quizzes`, quiz);
+  return response.data;
+};
+
+export const findAllQuizes = async () => {
+  const response = await axios.get(`${QUIZZES_API}/`);
+  return response.data;
+};
+
+export const findQuizById = async (qid: any) => {
+  const response = await axios.get(`${QUIZZES_API}/${qid}`);
+  return response.data;
+};
+
+export const findQuizByCourseId = async (courseId: any) => {
+  const response = await axios.get(`${COURSES_API}/${courseId}/quizzes`);
+  return response.data;
+};
+
+export const updateQuiz = async (quiz: any) => {
+  const response = await api.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  return response.data;
+};
+
+export const deleteQuiz = async (quiz: any) => {
+  const response = await api.delete(`${QUIZZES_API}/${quiz._id}`);
+  return response.data;
+};
+
+export const createQuestion = async (question: any) => {
+  const response = await api.post(`${QUESTIONS_API}`, question);
+  return response.data;
+};
+
+export const findQuestionsByQuizId = async (quizId: any) => {
+  const response = await axios.get(`${QUIZZES_API}/${quizId}/questions`);
+  return response.data;
+};
+
+export const findQuestionById = async (questionId: any) => {
+  const response = await axios.get(`${QUESTIONS_API}/${questionId}`);
+  return response.data;
+};
+
+export const updateQuestion = async (question: any) => {
+  const response = await api.put(`${QUESTIONS_API}/${question._id}`, question);
+  return response.data;
+};
+
+export const deleteQuestion = async (question: any) => {
+  const response = await api.delete(`${QUESTIONS_API}/${question._id}`);
+  return response.data;
+};
